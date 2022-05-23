6547fcf3b6cb766eb1ba93448e4ef2e2fd883c11
Jenna Hrdlicka <hrdlickajenna@gmail.com>
2022-05-23 14:39:58 -0600

    commit tree 472fde5e359543d84e65eaeab5df8e3c55f17d6d

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,19 @@
-import React from "react";
-import ProjectItem from "./ProjectItem";
+import React from 'react';
+import ProjectItem from './ProjectItem';
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  const projectListItems = projects.map((project) => (
+    <ProjectItem
+      key={project.id}
+      name={project.name}
+      about={project.about}
+      technologies={project.technologies}
+    />
+  ));
   return (
-    <div id="projects">
+    <div id='projects'>
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id='project-list'>{projectListItems}</div>
     </div>
   );
 }
